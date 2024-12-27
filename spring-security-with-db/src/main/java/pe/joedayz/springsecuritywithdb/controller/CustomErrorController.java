70363d810cc028867b0c39ef5273e18e5f0762af
Jose Amadeo Diaz Diaz <jose.diaz@joedayz.pe>
2024-12-26 20:38:11 -0500

    usando la nueva forma de manejar errores

--- spring-security-with-db/src/main/java/pe/joedayz/springsecuritywithdb/controller/CustomErrorController.java
+++ spring-security-with-db/src/main/java/pe/joedayz/springsecuritywithdb/controller/CustomErrorController.java
@@ -2,9 +2,11 @@ package pe.joedayz.springsecuritywithdb.controller;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,18 +14,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author josediaz
  **/
 @Controller
-@RequestMapping
-public class CustomErrorController implements ErrorController {
+@RequestMapping("/error")
+public class CustomErrorController {
 
-  @GetMapping("/error")
-  public String handleError(HttpServletRequest request) {
-    Object statusCode = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-    if(statusCode != null && Integer.parseInt(statusCode.toString()) == HttpStatus.FORBIDDEN.value()) {
-      return "403";
-    }else if (statusCode != null && Integer.parseInt(statusCode.toString()) == HttpStatus.NOT_FOUND.value()) {
-      return "404";
-    }
-    return "error";
+  private final ErrorAttributes errorAttributes;
+
+  public CustomErrorController(ErrorAttributes errorAttributes) {
+    this.errorAttributes = errorAttributes;
   }
 
+  @GetMapping
+  public String handleError(HttpServletRequest request, Model model) {
+    HttpStatus status = getStatus(request);
+    model.addAttribute("status", status.value());
+    model.addAttribute("error", status.getReasonPhrase());
+
+    if (status == HttpStatus.FORBIDDEN) {
+      return "403"; // Redirige a una página específica para 403
+    } else if (status == HttpStatus.NOT_FOUND) {
+      return "404"; // Redirige a una página específica para 404
+    }
+
+    return "error"; // Página genérica de error
+  }
+
+  private HttpStatus getStatus(HttpServletRequest request) {
+    Object statusCode = request.getAttribute("jakarta.servlet.error.status_code");
+    if (statusCode != null) {
+      try {
+        return HttpStatus.valueOf(Integer.parseInt(statusCode.toString()));
+      } catch (NumberFormatException ignored) {
+      }
+    }
+    return HttpStatus.INTERNAL_SERVER_ERROR;
+  }
 }
